495e82e5d67ddfe5756abed1a4e2d10c6651f980
la-magbanua <la.magbanua18@gmail.com>
2020-09-22 08:47:54 +0800

    adjusted theme colors

--- styles/theme.js
+++ styles/theme.js
@@ -5,10 +5,10 @@ export default {
     lightgrey: `#939da3`,
     white: `#fff`,
     offwhite: `#c1c7cb`,
-    itembg: `#181818`,
+    itembg: `#1e1e1e`,
     gradient: `linear-gradient(to right, #f64f59, #c471ed, #12c2e9)`,
     wgradient: ` -webkit-linear-gradient(to right, #f64f59, #c471ed, #12c2e9)`,
-    sgradient: `linear-gradient(-90deg, #111 0%, #222 50%, #111 100%)`,
+    sgradient: `linear-gradient(-90deg, #202020 0%, #383838 50%, #202020 100%)`,
   },
   fontWeights: {
     normal: 400,
